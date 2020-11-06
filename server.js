c235ad0539391c4f29467b241a7b14b246e0e6f1
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-06 20:12:31 +0000

    sorting the server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('mongoose')
 
 
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.json());
 
-app.listen(port, () => console.log())
+app.listen(port, () => console.log(`Server started on port: ${port}`))
