f7ae1694e90ea4e52a0f4b4899027a02745a13f0
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-07 20:26:54 +0000

    converter introduced

--- server/api/posts.js
+++ server/api/posts.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const Post = require('../models/Post');
+const showdown = require('showdown');
+converter = new showdown.Converter();
 
 router.get('/', (req, res) => {
   Post.find()
@@ -15,6 +17,17 @@ router.post('/', (req, res) => {
   });
   newPost.save().then(post => res.json(post));
 });
+
+
+// app.post("/convert", function(req, res, next) {
+//   if(typeof req.body.content == 'undefined' || req.body.content == null) {
+//       res.json(["error", "No data found"]);
+//   } else {
+//       text = req.body.content;
+//       html = converter.makeHtml(text);
+//       res.json(["markdown", html]);
+//   }
+// });
 
 router.delete('/:id', (req, res) => {
   Post.findById(req.params.id)
--- server/models/Post.js
+++ server/models/Post.js
@@ -3,18 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // const User;
 
-// const showdown = require('showdown')
-// converter = new showdown.Converter();
-
-// app.post("/convert", function(req, res, next) {
-//   if(typeof req.body.content == 'undefined' || req.body.content == null) {
-//       res.json(["error", "No data found"]);
-//   } else {
-//       text = req.body.content;
-//       html = converter.makeHtml(text);
-//       res.json(["markdown", html]);
-//   }
-// });
 
 
 const PostSchema = new Schema({
