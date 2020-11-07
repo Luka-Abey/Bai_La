52bfd6d37d4a7d0f99b73c53f04fbb27b7f460c1
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-07 18:23:07 +0000

    setting up mongodb

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const express = require('mongoose')
+const mongoose = require('mongoose')
 
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json());
+
+const db = require('keys.js').mongoURI;
+
+mongoose.connect(db)
+  .then(() => console.log('Mongo db connected...'))
 
 app.listen(port, () => console.log(`Server started on port: ${port}`))
