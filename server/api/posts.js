19f0b9b3b18c29f7d7dcdd113e2c359ffa13f11b
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-07 20:36:44 +0000

    update readme, markdown working

--- server/api/posts.js
+++ server/api/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Post = require('../models/Post');
 const showdown = require('showdown');
+
 converter = new showdown.Converter();
 
 router.get('/', (req, res) => {
@@ -13,21 +14,10 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const newPost = new Post({
-    postBody: req.body.postBody
+    postBody: converter.makeHtml(req.body.postBody)
   });
   newPost.save().then(post => res.json(post));
 });
-
-
-// app.post("/convert", function(req, res, next) {
-//   if(typeof req.body.content == 'undefined' || req.body.content == null) {
-//       res.json(["error", "No data found"]);
-//   } else {
-//       text = req.body.content;
-//       html = converter.makeHtml(text);
-//       res.json(["markdown", html]);
-//   }
-// });
 
 router.delete('/:id', (req, res) => {
   Post.findById(req.params.id)
