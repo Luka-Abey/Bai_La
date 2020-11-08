dc743fb96e53343082dd7fa5fb662870b8742356
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-08 21:30:11 +0000

    videos rendering on client

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -2,9 +2,7 @@
   <div>
     <h1>not a social media site</h1>
     <div v-bind:key="post.id" v-for="post in posts">
-      <PostItem v-html="post.postBody" />
-      <div>
-      <iframe width="800" src="https://www.youtube.com/embed/AZiJxNTdzwI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <PostItem v-html="post.postBody + post.video" />
     </div>
   </div>
 </template>
