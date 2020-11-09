b7968d52ca7f88742ee09fed627b266a7f7643d9
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-09 11:04:06 +0000

    filled page

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -2,22 +2,29 @@
   <div>
     <h1>not a social media site <img src="../../public/na.png"> </h1>
     <div v-bind:key="post.id" v-for="post in posts">
-      <PostItem v-html="post.postBody" />
+      <PostItem v-html="post.postBody"/>
+      <CommentItem v-html="comment.commentBody" />
       <span v-if="typeof post.video !== 'undefined'">
         <PostItem v-html="post.video" />
       </span>
+      <hr>    
+      <div v-bind:key="comment.id" v-for="comment in comments">
+        <CommentItem v-html="comment.commentBody" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import PostItem from './PostItem.vue'
+import PostItem from './PostItem.vue';
+import CommentItem from './CommentItem.vue';
 
 export default {
   name: "Posts",
   components: {
-    PostItem
+    PostItem,
+    CommentItem
     },
-  props: ["posts"]
+  props: ["posts", "comments"]
 }
 </script>
