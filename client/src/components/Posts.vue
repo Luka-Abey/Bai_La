3352abf88e69747933b91fd5d2ee25da69f52be6
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-11 17:18:21 +0000

    create, read, and delete on comments and posts

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -10,16 +10,17 @@
     </form>
     <div class = "post" v-bind:key="post.id" v-for="post in posts">
       <PostItem class = "post-text" v-html="post.postBody"/>
-      <span v-if="typeof post.video !== 'undefined'">
+      <span v-if="post.video !== '<iframe src=https://www.youtube.com/embed/ frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen></iframe>'">
         <PostItem v-html="post.video" />
       </span>
+      <button v-on:click="deletePost(post._id)" class="btn-warning"><img src='../../public/bin.png'></button>
       <hr>    
       
       <div class = "comment" v-bind:key="comment.id" v-for="comment in comments">
         <div v-if="post._id == comment.post">
         <Comments class ="comment-text" v-html="comment.commentBody" />
         <div>
-          <button v-on:click="deleteComment(comment._id)" class="btn-warning" type="submit"><img src='../../public/bin.png'></button>
+          <button v-on:click="deleteComment(comment._id)" class="btn-warning"><img src='../../public/bin.png'></button>
         </div>
         <hr>
         </div>
@@ -61,7 +62,7 @@ export default {
     sendPost() {
       axios.post(`${url}posts`, {
         postBody: this.newPost,
-        video: this.newVideo})
+        video: this.newVideo.trim()})
         .then(res => console.log(res))
         .catch(err => console.log(err));
     },
@@ -73,6 +74,12 @@ export default {
       })
       .then(this.newComment = '')
         .catch(err => console.log(err));
+    },
+
+    deletePost(refPost){
+      axios.delete(`${url}posts/${refPost}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
     },
 
     deleteComment(refComment){
