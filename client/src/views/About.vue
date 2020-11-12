c48ff4066fd2033d0d96638614d99fb4a777c72a
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-12 15:07:14 +0000

    terms of use

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -6,6 +6,7 @@
     <h4>No addictive infinite scroll.</h4>
     <ul type="disc">
       <li> Select how many posts you want to scroll for.</li>
+      <br>
       <li> Max 50 posts, you can define the number of posts you view whenever you log in.</li>
 
     <h4>No like buttons.</h4>
@@ -20,7 +21,8 @@
 
     <h4>Terms of use</h4>
     <ul type="disc">
-      <li> We will never sell your data. We hardly even store any.</li>
+      <li> We will never sell your data.</li>
+      <br>
       <li> Your details are stored safely and securely. Nonetheless, please do not use passwords you have used before.</li>
     </ul>
 
