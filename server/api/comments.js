c9e80f07b546b2ebc886fb63c4d276572c84e0dd
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-13 14:11:09 +0000

    working on users and authentication

--- server/api/comments.js
+++ server/api/comments.js
@@ -21,6 +21,17 @@ router.post('/', (req, res) => {
   newComment.save().then(comment => res.json(comment));
 });
 
+
+// PUT
+// router.put('/:id', (req, res) => {
+//   const newComment = new Comment({
+//     user: req.body.user,
+//     commentBody: converter.makeHtml(req.body.commentBody),
+//     post: req.body.post
+//   });
+//   newComment.save().then(comment => res.json(comment));
+// });
+
 router.delete('/:id', (req, res) => {
   Comment.findById(req.params.id)
     .then(comment => 
