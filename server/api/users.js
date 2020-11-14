62cdf254e5da4c5df21bb46abbb44db50cf33c29
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-14 19:21:55 +0000

    implementing auth with jwt - consider when we need auth

--- server/api/users.js
+++ server/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+// const checkAuth = require('../middleware/check-auth');
 
 router.get('/', (req, res) => {
   User.find()
