c3c2ad799816996fa8058fb23d175b3cd2798045
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-14 18:45:48 +0000

    jwt incoming

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
   User.find()
