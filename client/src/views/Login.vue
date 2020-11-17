a743cf11d6a20032efbeff32d14d5153690e5bf9
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-17 16:10:24 +0000

    adding login page

---
+++ client/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page input-form">
+    <form action="">
+       Username:
+       <input type ="text" v-model="username" placeholder="Username">
+        Password:
+        <input type="password" v-model="password" placeholder="Password">
+      <button>Log in</button>
+      New here?
+      <button>Sign up</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
