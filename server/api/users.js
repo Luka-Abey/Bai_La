22a438c7bc90302a0ded142ad57069683cb3b846
Karan Basu <karan.basu16@gmail.com>
2020-11-20 18:10:39 +0000

    login auth

--- server/api/users.js
+++ server/api/users.js
@@ -1,82 +1,61 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 // const checkAuth = require('../middleware/check-auth');
+// functions
+const findAndValidate = async (username, password) => {
+  const user = await User.find({ username })
+  if (user) {
+    const isValid = await bcrypt.compare(password, user.password)
+    return isValid ? user : false
+  } else {
+    return false
+  }
+}
 
+// routes
 router.get('/', (req, res) => {
-  User.find()
-    .then(users => res.json(users))
-});
+  User.find().then(users => res.json(users))
+})
 
 router.get('/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then(users => res.json(users))
-});
+  User.findById(req.params.id).then(users => res.json(users))
+})
 
 router.post('/signup', (req, res) => {
   bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (err) {
       return res.status(500).json({
         error: err
-      });
+      })
     } else {
       const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: hash
-      });
-      newUser.save()
+      })
+      newUser
+        .save()
         .then(result => {
-          res.status(201).json({success: true});
+          res.status(201).json({ success: true })
         })
-        .catch(err => res.status(500).json({ success: false}));
+        .catch(err => res.status(500).json({ success: false }))
     }
   })
-});
+})
 
-router.post('/login', (req, res) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if(user.length < 1){
-        return res.status(401).json({
-          message: 'not authorised'
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: 'not authorised'
-          });
-        }
-        if (result) {
-          const token = jwt.sign({
-            email: user[0].email,
-            user: user[0]._id
-          }, 
-          'secret', 
-          );
-          return res.status(200).json({
-            message: 'authorised',
-            token: token
-          });
-        }
-        res.status(401).json({
-        message: 'not authorised'
-        })
-      })
-    })
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await findAndValidate(username, password)
+  return user ? (res.status(200), user) : (res.status(401), false)
 })
 
 router.delete('/:id', (req, res) => {
   User.findById(req.params.id)
-    .then(user => 
-      user.remove()
-    .then(() => res.json({ success: true }))
-    )
-    .catch(err => res.status(500).json({ success: false }));
+    .then(user => user.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(500).json({ success: false }))
 })
 
-module.exports = router;
+module.exports = router
