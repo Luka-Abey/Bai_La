8190be6a55cc92e87650320b1b349bc56df1f807
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-11-21 16:59:18 +0000

    some notes on editpost

---
+++ client/src/components/EditPost.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+template needs:
+open as a pop up/layered component 
+input fields prepopulated with current values
+
+<script>
+import AddPost from '@/components/AddPost.vue';
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
