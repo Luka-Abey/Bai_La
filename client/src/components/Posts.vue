bd6f75ee43d8b0d487e0c72947785e866f560163
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-12-02 08:59:07 +0000

    moving new comments to new component

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -17,23 +17,23 @@
       
       <div class = "comment" v-bind:key="comment._id" v-for="comment in allComments">
         <div v-if="post._id == comment.post">
-        <div class ="comment-text" v-html="comment.commentBody" />
-          <div>
-            <button v-on:click="deleteComment(comment._id)" class="btn btn-warning"><img src='../../public/bin.png'></button>
+          <div class ="comment-text" v-html="comment.commentBody" />
+            <div>
+              <button v-on:click="deleteComment(comment._id)" class="btn btn-warning"><img src='../../public/bin.png'></button>
+            </div>
+            <hr>
           </div>
-        <hr>
         </div>
+        <!-- ADD COMMENT COMPONENT GOES HERE -->
+        <form :key="post._id" @submit.prevent="sendComment(post._id)">
+          <div class="input-form">
+            <input v-bind:key="post._id" type ="text" v-model="newComment" placeholder="Write comment here!">
+            <button type = "submit" class="btn btn-send"><img src='../../public/send.png'></button>
+          </div>
+        </form>
+        <hr class="thick-line">
       </div>
-      <form :key="post._id" @submit.prevent="sendComment(post._id)">
-        <!-- test build -->
-        <div class="input-form">
-          <input v-bind:key="post._id" type ="text" v-model="newComment" placeholder="Write comment here!">
-          <button type = "submit" class="btn btn-send"><img src='../../public/send.png'></button>
-        </div>
-      </form>
-      <hr class="thick-line">
     </div>
-  </div>
   
   </div>
 
