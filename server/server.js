1e23dcb6aed91d536e40c64d8a59a941da6a818c
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2020-12-07 19:09:07 +0000

    working on logins

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ const db = require('./keys.js').mongoURI
 // Avoid CORS error
 app.use(
   cors({
-    origin: [url],
+    origin: url,
     credentials: true
     // exposedHeaders: ['set-cookie']
   })
